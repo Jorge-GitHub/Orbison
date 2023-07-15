06a6960e3d8847eff1ae5b9e1327ec5d97a2bb9a
George Gonzalez <84981824+GeorgeGonzalez@users.noreply.github.com>
2023-07-15 01:49:43 -0500

    Initial commit.

---
+++ source/Orbison/Orbison.UI/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Orbison.UI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    /// <summary>
+    /// Save an unhanded exception.
+    /// </summary>
+    private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        //e.Exception.Save();
+        e.Handled = true;
+    }
+
+    /// <summary>
+    /// Method to run when the application closes.
+    /// </summary>
+    private void Application_Exit(object sender, ExitEventArgs e)
+    {
+        (Process.GetCurrentProcess()).Kill();
+    }
+}
